c0397addf493e4d686963ef167dbc11673b34221
cfen <chris.fenn@guardian.co.uk>
2018-03-01 16:24:14 +0000

    tweaks

--- src/js/app.js
+++ src/js/app.js
@@ -150,14 +150,6 @@ function addListeners(){
         this.style.display = "none";
         window.resize();
   
-  // if (this.classList.contains('bad')) {
-  //   // The box that we clicked has a class of bad so let's remove it and add the good class
-  //  this.classList.remove('bad');
-  //  this.classList.add('good');
-  // } else {
-  //   // The user obviously can't follow instructions so let's alert them of what is supposed to happen next
-  //   alert("You can proceed!");
-  // }
         });
 }
 
